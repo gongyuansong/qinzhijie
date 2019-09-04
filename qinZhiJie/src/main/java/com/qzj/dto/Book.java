cdeac3eb9203efd4825354d8e822d2a00530e749
YuanSongGong <YuanSongGong@DESKTOP-1BCMDRV>
2019-09-04 14:22:49 +0800

    update

--- qinZhiJie/src/main/java/com/qzj/dto/Book.java
+++ qinZhiJie/src/main/java/com/qzj/dto/Book.java
@@ -21,6 +21,17 @@ public class Book extends BaseTgEntity{
 
 	private Integer orderNum;
 	
+	private Integer status;
+	
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
 	public Integer getOrderNum() {
 		return orderNum;
 	}
