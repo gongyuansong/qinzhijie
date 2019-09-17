bc733f3aa606430997f7a2642a85779eab6b356f
YuanSongGong <YuanSongGong@DESKTOP-1BCMDRV>
2019-09-17 14:52:27 +0800

    add login

--- qinZhiJie/src/main/java/com/qzj/controller/StationController.java
+++ qinZhiJie/src/main/java/com/qzj/controller/StationController.java
@@ -1,6 +1,8 @@
 package com.qzj.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -38,13 +40,18 @@ public class StationController extends BaseTgController {
 		book.setBookName(queryStr);
 		bookPage.setQueryObj(book);
 		List<Book> bookList = bookService.getList(bookPage).getDataList();
-		
-		
+		for(Book b : bookList) {
+			b.setDirPath("");
+		}
 		PageRequest<BookLyric> bookLyricPage = new PageRequest<>();
 		BookLyric bookLyric = new BookLyric();
 		bookLyric.setDesc(queryStr);
 		bookLyricPage.setQueryObj(bookLyric);
 		List<BookLyric> bookLyricList = bookLyricService.getList(bookLyricPage).getDataList();
+		for(BookLyric b : bookLyricList) {
+			b.setDesc("");
+			b.setUrl("");
+		}
 		
 		Station station = new Station();
 		station.setBookList(bookList);
@@ -55,4 +62,39 @@ public class StationController extends BaseTgController {
 		result.setMessage("Success");
 		return result;
 	}
+	
+	
+	@RequestMapping(value = "/searchStatus", method = RequestMethod.POST)
+	public ResponseData<Map<String, Integer>> searchStatus(@RequestParam("queryStr") String queryStr, HttpServletRequest request) {
+		ResponseData<Map<String, Integer>> result = new ResponseData<Map<String, Integer>>();
+		PageRequest<Book> bookPage = new PageRequest<>();
+		Map<String, Integer> resultMap = new HashMap<>();
+		Book book = new Book();
+		book.setBookName(queryStr);
+		bookPage.setQueryObj(book);
+		List<Book> bookList = bookService.getList(bookPage).getDataList();
+		
+		if(bookList != null && bookList.size() != 0) {
+			resultMap.put("liDaiQinPuStatus", 1);
+		}else {
+			resultMap.put("liDaiQinPuStatus", 0);
+		}
+		
+		PageRequest<BookLyric> bookLyricPage = new PageRequest<>();
+		BookLyric bookLyric = new BookLyric();
+		bookLyric.setDesc(queryStr);
+		bookLyricPage.setQueryObj(bookLyric);
+		List<BookLyric> bookLyricList = bookLyricService.getList(bookLyricPage).getDataList();
+		if(bookLyricList != null && bookLyricList.size() != 0) {
+			resultMap.put("qinquStatus", 1);
+		}else {
+			resultMap.put("qinquStatus", 0);
+		}
+		resultMap.put("qinshengStatus", 0);
+		result.setData(resultMap);
+		result.setCode("200");
+		result.setMessage("Success");
+		return result;
+	}
+	
 }
